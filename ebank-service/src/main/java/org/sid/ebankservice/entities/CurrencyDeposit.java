a7ffcae718482f4b9ae9138ff4a7f9df48f81225
SakuraMH <132273042+SakuraMH@users.noreply.github.com>
2023-12-03 00:15:41 +0100

    Add files via upload

---
+++ ebank-service/src/main/java/org/sid/ebankservice/entities/CurrencyDeposit.java
@@ -0,0 +1,25 @@
+package org.sid.ebankservice.entities;
+
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
+@Entity
+@Data @AllArgsConstructor @NoArgsConstructor @Builder
+public class CurrencyDeposit {
+    @Id
+    private String currencyId;
+    private double saleCurrencyPrice;
+    private double purchaseCurrencyPrice;
+    private double balance;
+    @OneToMany(mappedBy = "currencyDeposit")
+    private List<CurrencyTransaction> currencyTransactions;
+}
